Added sound level normalization, fixed transcribe, various minor improvements.

data/data_loader.py
@@ -9,6 +9,7 @@ from torch.utils.data.sampler import Sampler
 import librosa
 import numpy as np
 import scipy.signal
+import scipy.ndimage
 import torch
 import torchaudio
 import math
@@ -82,7 +83,7 @@ class NoiseInjection(object):
 
 
 class SpectrogramParser(AudioParser):
-    def __init__(self, audio_conf, normalize=False, augment=False):
+    def __init__(self, audio_conf, normalize=False, augment=False, normalize_by_frame=False):
         """
         Parses audio file into spectrogram with optional normalization and various augmentations
         :param audio_conf: Dictionary containing the sample rate, window and the window length/stride in seconds
@@ -95,6 +96,7 @@ class SpectrogramParser(AudioParser):
         self.sample_rate = audio_conf['sample_rate']
         self.window = windows.get(audio_conf['window'], windows['hamming'])
         self.normalize = normalize
+        self.normalize_by_frame = normalize_by_frame
         self.augment = augment
         self.noiseInjector = NoiseInjection(audio_conf['noise_dir'], self.sample_rate,
                                             audio_conf['noise_levels']) if audio_conf.get(
@@ -110,9 +112,9 @@ class SpectrogramParser(AudioParser):
             add_noise = np.random.binomial(1, self.noise_prob)
             if add_noise:
                 y = self.noiseInjector.inject_noise(y)
-        n_fft = int(self.sample_rate * (self.window_size+1e-8))
+        n_fft = int(self.sample_rate * (self.window_size + 1e-8))
         win_length = n_fft
-        hop_length = int(self.sample_rate * (self.window_stride+1e-8))
+        hop_length = int(self.sample_rate * (self.window_stride + 1e-8))
         # print(n_fft, win_length, hop_length)
         # STFT
         D = librosa.stft(y, n_fft=n_fft, hop_length=hop_length,
@@ -123,10 +125,14 @@ class SpectrogramParser(AudioParser):
         spect = torch.FloatTensor(spect)
         if self.normalize:
             mean = spect.mean()
-            std = spect.std()
             spect.add_(-mean)
-            spect.div_(std)
-
+        if self.normalize_by_frame:
+            mean = spect.mean(dim=0, keepdim=True)
+            # std = spect.std(dim=0, keepdim=True)
+            mean = torch.FloatTensor(scipy.ndimage.filters.gaussian_filter1d(mean.numpy(), 50))
+            # std = torch.FloatTensor(scipy.ndimage.filters.gaussian_filter1d(std.numpy(), 20))
+            spect.add_(-mean)
+            # spect.div_(std + 1e-8)
         return spect
 
     def parse_transcript(self, transcript_path):
@@ -134,7 +140,8 @@ class SpectrogramParser(AudioParser):
 
 
 class SpectrogramDataset(Dataset, SpectrogramParser):
-    def __init__(self, audio_conf, manifest_filepath, labels, normalize=False, augment=False, max_items=None):
+    def __init__(self, audio_conf, manifest_filepath, labels, normalize=False, augment=False,
+                 normalize_by_frame=False, max_items=None):
         """
         Dataset that loads tensors via a csv containing file paths to audio files and transcripts separated by
         a comma. Each new line is a different sample. Example below:
@@ -158,7 +165,7 @@ class SpectrogramDataset(Dataset, SpectrogramParser):
         self.size = len(ids)
         self.labels = labels
         self.labels_map = dict([(labels[i], i) for i in range(len(labels))])
-        super(SpectrogramDataset, self).__init__(audio_conf, normalize, augment)
+        super(SpectrogramDataset, self).__init__(audio_conf, normalize, augment, normalize_by_frame)
 
     def __getitem__(self, index):
         sample = self.ids[index]
@@ -181,6 +188,8 @@ class SpectrogramDataset(Dataset, SpectrogramParser):
     }
 
     def getch(self, c):
+        if c == 'Ё':
+            return 'Е'
         if c.isdigit():
             return ' ' + self.DIGITS[c] + ' '
         if c == '*':
@@ -197,8 +206,11 @@ class SpectrogramDataset(Dataset, SpectrogramParser):
             for c in ' '.join(chars.split()):
                 code = self.labels_map[c]
                 if transcript and transcript[-1] == code:
-                    code = self.labels_map['2']  # double char
+                    continue  # FIXME: for this special mode
+                    # code = self.labels_map['2']  # double char
                 transcript.append(code)
+        if len(transcript) < 1:
+            transcript = [self.labels_map['*']]
         # print(''.join([self.labels[c] for c in transcript]))
         return transcript
 
@@ -304,7 +316,7 @@ class DistributedBucketingSampler(Sampler):
 
 
 def get_audio_length(path):
-    output = subprocess.check_output(['soxi -D \"%s\"' % path.strip().replace('"','\\"')], shell=True)
+    output = subprocess.check_output(['soxi -D \"%s\"' % path.strip().replace('"', '\\"')], shell=True)
     return float(output)
 
 
@@ -314,9 +326,9 @@ def audio_with_sox(path, sample_rate, start_time, end_time):
     """
     with NamedTemporaryFile(suffix=".wav") as tar_file:
         tar_filename = tar_file.name
-        sox_params = "sox \"{}\" -r {} -c 1 -b 16 -e si {} trim {} ={} >>sox.1.log 2>>sox.2.log".format(path.replace('"', '\\"'), sample_rate,
-                                                                                               tar_filename, start_time,
-                                                                                               end_time)
+        sox_params = "sox \"{}\" -r {} -c 1 -b 16 -e si {} trim {} ={} >>sox.1.log 2>>sox.2.log".format(
+            path.replace('"', '\\"'), sample_rate,
+            tar_filename, start_time, end_time)
         os.system(sox_params)
         y = load_audio(tar_filename)
         return y
@@ -329,15 +341,17 @@ def augment_audio_with_sox(path, sample_rate, tempo, gain):
     with NamedTemporaryFile(suffix=".wav") as augmented_file:
         augmented_filename = augmented_file.name
         sox_augment_params = ["tempo", "{:.3f}".format(tempo), "gain", "{:.3f}".format(gain)]
-        sox_params = "sox \"{}\" -r {} -c 1 -b 16 -e si {} {} >>sox.1.log 2>>sox.2.log".format(path.replace('"', '\\"'), sample_rate,
-                                                                                      augmented_filename,
-                                                                                      " ".join(sox_augment_params))
+        sox_params = "sox \"{}\" -r {} -c 1 -b 16 -e si {} {} >>sox.1.log 2>>sox.2.log".format(
+            path.replace('"', '\\"'),
+            sample_rate,
+            augmented_filename,
+            " ".join(sox_augment_params))
         os.system(sox_params)
         y = load_audio(augmented_filename)
         return y
 
 
-def load_randomly_augmented_audio(path, sample_rate=16000, tempo_range=(0.85, 1.15),
+def load_randomly_augmented_audio(path, sample_rate=16000, tempo_range=(0.6, 1.6),
                                   gain_range=(-6, 8)):
     """
     Picks tempo and gain uniformly, applies it to the utterance by using sox utility.

model.py
@@ -4,8 +4,8 @@ from collections import OrderedDict
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn.parameter import Parameter
 from torch.autograd import Variable
+from torch.nn.parameter import Parameter
 
 supported_rnns = {
     'lstm': nn.LSTM,
@@ -103,7 +103,7 @@ class BatchRNN(nn.Module):
         x, _ = nn.utils.rnn.pad_packed_sequence(x, total_length=max_seq_length)
         if self.bidirectional:
             x = x.view(x.size(0), x.size(1), 2, -1).sum(2).view(x.size(0), x.size(1), -1)  # (TxNxH*2) -> (TxNxH) by sum
-        #x = x.to('cuda')
+        # x = x.to('cuda')
         return x
 
 
@@ -196,13 +196,15 @@ class DeepSpeech(nn.Module):
         window_size = self._audio_conf.get("window_size", 0.02)
         num_classes = len(self._labels)
 
+        self.dropout1 = nn.Dropout(p=0.1, inplace=True)
+        self.dropout2 = nn.Dropout(p=0.1, inplace=True)
         self.conv = MaskConv(nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=(41, 11), stride=(2, 2), padding=(20, 5)),
             nn.BatchNorm2d(32, momentum=bnm),
             nn.Hardtanh(0, 20, inplace=True),
             nn.Conv2d(32, 32, kernel_size=(21, 11), stride=(2, 1), padding=(10, 5)),
             nn.BatchNorm2d(32, momentum=bnm),
-            nn.Hardtanh(0, 20, inplace=True)
+            nn.Hardtanh(0, 20, inplace=True),
         ))
         # Based on above convolutions and spectrogram size using conv formula (W - F + 2P)/ S+1
         rnn_input_size = int(math.floor((sample_rate * window_size + 1e-2) / 2) + 1)
@@ -238,9 +240,11 @@ class DeepSpeech(nn.Module):
         assert x.is_cuda
         lengths = lengths.cpu().int()
         output_lengths = self.get_seq_lens(lengths).cuda()
+        x = self.dropout1(x)
         x, _ = self.conv(x, output_lengths)
+        x = self.dropout2(x)
         assert x.is_cuda
-        #x = x.to('cuda')
+        # x = x.to('cuda')
 
         sizes = x.size()
         x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  # Collapse feature dimension

test.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
         decoder = None
     target_decoder = GreedyDecoder(labels, blank_index=labels.index('_'))
     test_dataset = SpectrogramDataset(audio_conf=audio_conf, manifest_filepath=args.test_manifest, labels=labels,
-                                      normalize=False)
+                                      normalize_by_frame=True)
     #import random;random.shuffle(test_dataset.ids)
 
     test_loader = AudioDataLoader(test_dataset, batch_size=args.batch_size,
@@ -66,6 +66,7 @@ if __name__ == '__main__':
             offset += size
 
         inputs = inputs.to(device)
+
         out, output_sizes = model(inputs, input_sizes)
 
         del inputs, targets, input_percentages, target_sizes
@@ -87,8 +88,8 @@ if __name__ == '__main__':
                 print("Wav:", filenames[x])
                 print("WER:", "{:.2f}".format(100 * wer / wer_ref), "CER:", "{:.2f}".format(100 * cer / cer_ref), "\n")
             elif args.errors:
-                if cer / cer_ref > 0.3:
-                    #print("FN:", )
+                if cer / cer_ref > 0.5:
+                    # print("FN:", )
                     print("Ref:", reference)
                     print("Hyp:", transcript)
                     print("Wav:", filenames[x])
@@ -99,9 +100,8 @@ if __name__ == '__main__':
             num_tokens += wer_ref
             num_chars += cer_ref
 
-
         del out, output_sizes
-        if (x+1) % 20 == 0:
+        if (i+1) % 5 == 0:
             gc.collect()
             torch.cuda.empty_cache()
 

train.py
@@ -284,12 +284,12 @@ def set_lr(lr):
     optimizer.load_state_dict(optim_state)
 
 
-def check_model_quality(epoch, checkpoint):
+def check_model_quality(epoch, checkpoint, train_loss):
     gc.collect()
     torch.cuda.empty_cache()
 
-    total_cer, total_wer, total_loss = 0, 0, 0
-    num_tokens, num_chars, num_losses = 0, 0, 0
+    valid_cer, valid_wer, valid_loss = 0, 0, 0
+    num_chars, num_tokens, num_losses = 0, 0, 0
     model.eval()
     with torch.no_grad():
         for i, data in tqdm(enumerate(test_loader), total=len(test_loader)):
@@ -317,7 +317,9 @@ def check_model_quality(epoch, checkpoint):
             if loss_value == inf or loss_value == -inf:
                 print("WARNING: received an inf loss, setting loss value to 1000")
                 loss_value = 1000
-            total_loss += float(loss_value)
+            loss_value = float(loss_value)
+            valid_loss = (valid_loss * 0.998 + loss_value * 0.002)  # discount earlier losses
+            valid_loss += loss_value
             num_losses += 1
 
             decoded_output, _ = decoder.decode(out, output_sizes)
@@ -326,8 +328,8 @@ def check_model_quality(epoch, checkpoint):
                 transcript, reference = decoded_output[x][0], target_strings[x][0]
                 wer, cer, wer_ref, cer_ref = get_cer_wer(decoder, transcript, reference)
 
-                total_wer += wer
-                total_cer += cer
+                valid_wer += wer
+                valid_cer += cer
                 num_tokens += wer_ref
                 num_chars += cer_ref
 
@@ -338,19 +340,19 @@ def check_model_quality(epoch, checkpoint):
             if args.cuda:
                 torch.cuda.synchronize()
 
-        avg_wer = 100 * total_wer / num_tokens
-        avg_cer = 100 * total_cer / num_chars
+        avg_wer = 100 * valid_wer / num_tokens
+        avg_cer = 100 * valid_cer / num_chars
         print('Validation Summary Epoch: [{0}]\t'
               'Average WER {wer:.3f}\t'
               'Average CER {cer:.3f}\t'.format(epoch + 1, wer=avg_wer, cer=avg_cer))
 
-        avg_loss = total_loss / num_losses
-        plots.loss_results[epoch] = avg_loss
+        avg_loss = valid_loss / num_losses
+        plots.loss_results[epoch] = train_loss
         plots.wer_results[epoch] = avg_wer
         plots.cer_results[epoch] = avg_cer
         plots.epochs[epoch] = epoch+1
 
-        checkpoint_plots.loss_results[checkpoint] = avg_loss
+        checkpoint_plots.loss_results[checkpoint] = train_loss
         checkpoint_plots.wer_results[checkpoint] = avg_wer
         checkpoint_plots.cer_results[checkpoint] = avg_cer
         checkpoint_plots.epochs[checkpoint] = checkpoint+1
@@ -358,7 +360,7 @@ def check_model_quality(epoch, checkpoint):
         plots.plot_progress(epoch, avg_loss, avg_cer, avg_wer)
         checkpoint_plots.plot_progress(checkpoint, avg_loss, avg_cer, avg_wer)
 
-        if args.checkpoint_anneal:
+        if args.checkpoint_anneal != 1.0:
             global lr_plots
             lr_plots.loss_results[checkpoint] = avg_loss
             lr_plots.epochs[checkpoint:] = get_lr()
@@ -385,7 +387,10 @@ class Trainer:
         assert output_sizes.is_cuda
         out = out.transpose(0, 1)  # TxNxH
 
-        out[torch.isnan(out)] = 0
+        if torch.isnan(out).any():  # and args.nan == 'zero':
+            # work around bad data
+            print("WARNING: Working around NaNs in data")
+            out[torch.isnan(out)] = 0
 
         loss = criterion(out, targets, output_sizes.cpu(), target_sizes)
         loss = loss / inputs.size(0)  # average the loss by minibatch
@@ -400,6 +405,7 @@ class Trainer:
             print("WARNING: received an inf loss, setting loss value to 1000")
             loss_value = 1000
 
+        loss_value = float(loss_value)
         losses.update(loss_value, inputs.size(0))
 
         # compute gradient
@@ -407,9 +413,10 @@ class Trainer:
         loss.backward()
 
         torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_norm)
+
         if torch.isnan(out).any():
             # work around bad data
-            print("Skipping NaN backward step")
+            print("WARNING: Skipping NaNs in backward step")
         else:
             # SGD step
             optimizer.step()
@@ -469,7 +476,7 @@ def train(from_epoch, from_iter, from_checkpoint):
                                                     checkpoint_cer_results=checkpoint_plots.cer_results,
                                                     avg_loss=total_loss / num_losses), file_path)
 
-                    check_model_quality(epoch, checkpoint)
+                    check_model_quality(epoch, checkpoint, total_loss / num_losses)
                     checkpoint += 1
                     model.train()
                     if args.checkpoint_anneal != 1:
@@ -485,7 +492,7 @@ def train(from_epoch, from_iter, from_checkpoint):
               'Average Loss {loss:.3f}\t'.format(epoch + 1, epoch_time=epoch_time, loss=total_loss / num_losses))
 
         from_iter = 0  # Reset start iteration for next epoch
-        wer_avg = check_model_quality(epoch, checkpoint)
+        wer_avg = check_model_quality(epoch, checkpoint, total_loss / num_losses)
         checkpoint += 1
 
         if args.checkpoint and is_leader:  # checkpoint after the end of each epoch
@@ -608,10 +615,10 @@ if __name__ == '__main__':
     decoder = GreedyDecoder(labels)
     train_dataset = SpectrogramDataset(audio_conf=audio_conf,
                                        manifest_filepath=args.train_manifest,
-                                       labels=labels, normalize=False, augment=args.augment)
+                                       labels=labels, normalize_by_frame=True, augment=args.augment)
     test_dataset = SpectrogramDataset(audio_conf=audio_conf,
                                       manifest_filepath=args.val_manifest,
-                                      labels=labels, normalize=False, augment=False)
+                                      labels=labels, normalize_by_frame=True, augment=False)
     if args.reverse_sort:
         # XXX: A hack to test max memory load.
         train_dataset.ids.reverse()
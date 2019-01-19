63775c388cbc845b93100a63c60f85a89eae0886
Yuri Baburov <burchik@gmail.com>
2019-01-20 00:59:00 +0700

    Added reporting WER, CER and transcripts to a file.

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import argparse
+import csv
+import os
 
 import numpy as np
 import torch
@@ -19,6 +21,8 @@ parser.add_argument('--batch-size', default=20, type=int, help='Batch size for t
 parser.add_argument('--num-workers', default=4, type=int, help='Number of workers used in dataloading')
 parser.add_argument('--verbose', action="store_true", help="print out decoded output and error of each sample")
 parser.add_argument('--errors', action="store_true", help="print error report")
+parser.add_argument('--best', action="store_true", help="print best results")
+parser.add_argument('--report-file', metavar='DIR', default='data/test_report.csv', help="Filename to save results")
 no_decoder_args = parser.add_argument_group("No Decoder Options", "Configuration options for when no decoder is "
                                                                   "specified")
 no_decoder_args.add_argument('--output-path', default=None, type=str, help="Where to save raw acoustic output")
@@ -35,6 +39,12 @@ if __name__ == '__main__':
     labels = DeepSpeech.get_labels(model)
     audio_conf = DeepSpeech.get_audio_conf(model)
 
+    report_file = None
+    if args.report_file:
+        os.makedirs(os.path.dirname(args.report_file), exist_ok=True)
+        report_file = csv.writer(open(args.report_file, 'wt'))
+        report_file.writerow(['wav', 'text', 'transcript', 'offsets', 'CER', 'WER'])
+
     if args.decoder == "beam":
         from decoder import BeamCTCDecoder
 
@@ -46,9 +56,11 @@ if __name__ == '__main__':
     else:
         decoder = None
     target_decoder = GreedyDecoder(labels, blank_index=labels.index('_'))
-    test_dataset = SpectrogramDataset(audio_conf=audio_conf, manifest_filepath=args.test_manifest, labels=labels,
+    test_dataset = SpectrogramDataset(audio_conf=audio_conf,
+                                      manifest_filepath=args.test_manifest,
+                                      labels=labels,
                                       normalize_by_frame=True)
-    #import random;random.shuffle(test_dataset.ids)
+    # import random;random.shuffle(test_dataset.ids)
 
     test_loader = AudioDataLoader(test_dataset, batch_size=args.batch_size,
                                   num_workers=args.num_workers)
@@ -88,12 +100,33 @@ if __name__ == '__main__':
                 print("Wav:", filenames[x])
                 print("WER:", "{:.2f}".format(100 * wer / wer_ref), "CER:", "{:.2f}".format(100 * cer / cer_ref), "\n")
             elif args.errors:
-                if cer / cer_ref > 0.5:
+                if cer / cer_ref > 0.5 and transcript.strip():
                     # print("FN:", )
                     print("Ref:", reference)
                     print("Hyp:", transcript)
                     print("Wav:", filenames[x])
-                    print("WER:", "{:.2f}".format(100 * wer / wer_ref), "CER:", "{:.2f}".format(100 * cer / cer_ref), "\n")
+                    print("WER:", "{:.2f}".format(100 * wer / wer_ref), "CER:", "{:.2f}".format(100 * cer / cer_ref),
+                          "\n")
+            elif args.best:
+                if cer / cer_ref < 0.15:
+                    # print("FN:", )
+                    print("Ref:", reference)
+                    print("Hyp:", transcript)
+                    print("Wav:", filenames[x])
+                    print("WER:", "{:.2f}".format(100 * wer / wer_ref), "CER:", "{:.2f}".format(100 * cer / cer_ref),
+                          "\n")
+
+            if report_file:
+                # report_file.write_row(['wav', 'text', 'transcript', 'offsets', 'CER', 'WER'])
+
+                report_file.writerow([
+                    filenames[x],
+                    reference,
+                    transcript,
+                    '',
+                    cer / cer_ref,
+                    wer / wer_ref
+                ])
 
             total_wer += wer
             total_cer += cer
@@ -101,7 +134,7 @@ if __name__ == '__main__':
             num_chars += cer_ref
 
         del out, output_sizes
-        if (i+1) % 5 == 0:
+        if (i + 1) % 5 == 0:
             gc.collect()
             torch.cuda.empty_cache()
 

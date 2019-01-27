Added wav2letter model (use option --rnn-type=cnn), fixed validation loss calculation.

data/data_loader.py
@@ -86,7 +86,7 @@ class NoiseInjection(object):
 
 
 class SpectrogramParser(AudioParser):
-    def __init__(self, audio_conf, normalize=False, augment=False):
+    def __init__(self, audio_conf, normalize=False, augment=False, channel=-1):
         """
         Parses audio file into spectrogram with optional normalization and various augmentations
         :param audio_conf: Dictionary containing the sample rate, window and the window length/stride in seconds
@@ -100,6 +100,7 @@ class SpectrogramParser(AudioParser):
         self.window = windows.get(audio_conf['window'], windows['hamming'])
         self.normalize = normalize
         self.augment = augment
+        self.channel = channel
         self.noiseInjector = NoiseInjection(audio_conf['noise_dir'], self.sample_rate,
                                             audio_conf['noise_levels']) if audio_conf.get(
             'noise_dir') is not None else None
@@ -107,9 +108,9 @@ class SpectrogramParser(AudioParser):
 
     def parse_audio(self, audio_path):
         if self.augment:
-            y = load_randomly_augmented_audio(audio_path, self.sample_rate)
+            y = load_randomly_augmented_audio(audio_path, self.sample_rate, channel=self.channel)
         else:
-            y = load_audio(audio_path)
+            y = load_audio(audio_path, channel=self.channel)
         if self.noiseInjector:
             add_noise = np.random.binomial(1, self.noise_prob)
             if add_noise:
@@ -120,13 +121,15 @@ class SpectrogramParser(AudioParser):
         return spect
 
     def parse_audio_for_transcription(self, audio_path, channel):
-        #y = load_audio(audio_path, channel=channel)
-        y = load_randomly_augmented_audio(audio_path, tempo_range=(0.5, 0.5), gain_range=(-5,-5))
+        # y = load_audio(audio_path, channel=channel)
+        y = load_randomly_augmented_audio(audio_path, tempo_range=(1, 1), gain_range=(-10, -10), channel=self.channel)
         spect = self.audio_to_stft(y)
-        #print(spect.shape, spect.shape[1] / 50)
-        spect = spect[:, :1000].copy()
+        # print(spect.shape, spect.shape[1] / 50)
+        # print(spect.shape)
+        # spect = spect[:, :1200].copy()
+        # print(spect.shape)
         spect = self.normalize_audio(spect)
-        #spect.add_(-2)
+        # spect.add_(-2)
         return spect
 
     def audio_to_stft(self, y):
@@ -205,7 +208,7 @@ class SpectrogramDataset(Dataset, SpectrogramParser):
             ids = [(row[0], row[1], row[2] if len(row) > 2 else 0) for row in reader]
         if max_items:
             ids = ids[:max_items]
-        print("Found entries:", len(ids))
+        #print("Found entries:", len(ids))
         # self.all_ids = ids
         self.curriculum = None
         self.all_ids = ids
@@ -216,14 +219,18 @@ class SpectrogramDataset(Dataset, SpectrogramParser):
         if curriculum_filepath:
             with open(curriculum_filepath, newline='') as f:
                 reader = csv.DictReader(f)
-                self.curriculum = {row['wav']: row for row in reader}
+                rows = [row for row in reader]
+                for r in rows:
+                    r['cer'] = float(r['cer'])
+                    r['wer'] = float(r['wer'])
+                self.curriculum = {row['wav']: row for row in rows}
         else:
             self.curriculum = {wav: {'wav': wav,
                                      'text': self.get_reference_transcript(txt),
                                      'transcript': '',
                                      'offsets': None,
-                                     'cer': 1,
-                                     'wer': 1} for wav, txt, dur in ids}
+                                     'cer': 0.999,
+                                     'wer': 0.999} for wav, txt, dur in ids}
         super(SpectrogramDataset, self).__init__(audio_conf, normalize, augment)
 
     def __getitem__(self, index):
@@ -235,6 +242,8 @@ class SpectrogramDataset(Dataset, SpectrogramParser):
 
     def get_curriculum_info(self, item):
         audio_path, transcript_path, _dur = item
+        if audio_path not in self.curriculum:
+            return self.get_reference_transcript(transcript_path), 0.999
         return self.curriculum[audio_path]['text'], self.curriculum[audio_path]['cer']
 
     def set_curriculum_epoch(self, epoch, sample=False):
@@ -427,14 +436,16 @@ def audio_with_sox(path, sample_rate, start_time, end_time):
         return y
 
 
-def augment_audio_with_sox(path, sample_rate, tempo, gain):
+def augment_audio_with_sox(path, sample_rate, tempo, gain, channel=-1):  # channels: -1 = both, 0 = left, 1 = right
     """
     Changes tempo and gain of the recording with sox and loads it.
     """
     with NamedTemporaryFile(suffix=".wav") as augmented_file:
         augmented_filename = augmented_file.name
         sox_augment_params = ["tempo", "{:.3f}".format(tempo), "gain", "{:.3f}".format(gain)]
-        sox_params = "sox \"{}\" -r {} -c 1 -b 16 -e si {} {} >>sox.1.log 2>>sox.2.log".format(
+        if channel != -1:
+            sox_augment_params.extend(["remix", str(channel + 1)])
+        sox_params = "sox \"{}\" -r {} -c 1 -b 16 -t wav -e si {} {} >>sox.1.log 2>>sox.2.log".format(
             path.replace('"', '\\"'),
             sample_rate,
             augmented_filename,
@@ -444,8 +455,8 @@ def augment_audio_with_sox(path, sample_rate, tempo, gain):
         return y
 
 
-def load_randomly_augmented_audio(path, sample_rate=16000, tempo_range=(0.9, 1.1),
-                                  gain_range=(-10, 10)):
+def load_randomly_augmented_audio(path, sample_rate=16000, tempo_range=(0.85, 1.15),
+                                  gain_range=(-10, 10), channel=-1):
     """
     Picks tempo and gain uniformly, applies it to the utterance by using sox utility.
     Returns the augmented utterance.
@@ -455,5 +466,5 @@ def load_randomly_augmented_audio(path, sample_rate=16000, tempo_range=(0.9, 1.1
     low_gain, high_gain = gain_range
     gain_value = np.random.uniform(low=low_gain, high=high_gain)
     audio = augment_audio_with_sox(path=path, sample_rate=sample_rate,
-                                   tempo=tempo_value, gain=gain_value)
+                                   tempo=tempo_value, gain=gain_value, channel=channel)
     return audio

model.py
@@ -10,7 +10,8 @@ from torch.nn.parameter import Parameter
 supported_rnns = {
     'lstm': nn.LSTM,
     'rnn': nn.RNN,
-    'gru': nn.GRU
+    'gru': nn.GRU,
+    'cnn': None
 }
 supported_rnns_inv = dict((v, k) for k, v in supported_rnns.items())
 
@@ -67,14 +68,6 @@ class MaskConv(nn.Module):
                     mask[i].narrow(2, length, mask[i].size(2) - length).fill_(1)
             x = x.masked_fill(mask, 0)
         return x, lengths
-
-
-class InferenceBatchSoftmax(nn.Module):
-    def forward(self, input_):
-        if not self.training:
-            return F.softmax(input_, dim=-1)
-        else:
-            return input_
 
 
 class BatchRNN(nn.Module):
@@ -175,6 +168,9 @@ class Lookahead(nn.Module):
                + ', context=' + str(self.context) + ')'
 
 
+DEBUG = 1
+
+
 class DeepSpeech(nn.Module):
     def __init__(self, rnn_type=nn.LSTM, labels="abc", rnn_hidden_size=768, nb_layers=5, audio_conf=None,
                  bidirectional=True, context=20, bnm=0.1):
@@ -196,8 +192,8 @@ class DeepSpeech(nn.Module):
         window_size = self._audio_conf.get("window_size", 0.02)
         num_classes = len(self._labels)
 
-        self.dropout1 = nn.Dropout(p=0.1, inplace=True)
-        self.dropout2 = nn.Dropout(p=0.1, inplace=True)
+        self.dropout1 = nn.Dropout(p=0.5, inplace=True)
+        self.dropout2 = nn.Dropout(p=0.5, inplace=True)
         self.conv = MaskConv(nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=(41, 11), stride=(2, 2), padding=(20, 5)),
             nn.BatchNorm2d(32, momentum=bnm),
@@ -206,67 +202,104 @@ class DeepSpeech(nn.Module):
             nn.BatchNorm2d(32, momentum=bnm),
             nn.Hardtanh(0, 20, inplace=True),
         ))
-        # Based on above convolutions and spectrogram size using conv formula (W - F + 2P)/ S+1
-        rnn_input_size = int(math.floor((sample_rate * window_size + 1e-2) / 2) + 1)
-        rnn_input_size = int(math.floor(rnn_input_size + 2 * 20 - 41 + 1e-2) / 2 + 1)
-        rnn_input_size = int(math.floor(rnn_input_size + 2 * 10 - 21 + 1e-2) / 2 + 1)
-        rnn_input_size *= 32
 
-        rnns = []
-        rnn = BatchRNN(input_size=rnn_input_size, hidden_size=rnn_hidden_size, rnn_type=rnn_type,
-                       bidirectional=bidirectional, batch_norm=False)
-        rnns.append(('0', rnn))
-        for x in range(nb_layers - 1):
-            rnn = BatchRNN(input_size=rnn_hidden_size, hidden_size=rnn_hidden_size, rnn_type=rnn_type,
-                           bidirectional=bidirectional, bnm=bnm)
-            rnns.append(('%d' % (x + 1), rnn))
-        self.rnns = nn.Sequential(OrderedDict(rnns))
-        self.lookahead = nn.Sequential(
-            # consider adding batch norm?
-            Lookahead(rnn_hidden_size, context=context),
-            nn.Hardtanh(0, 20, inplace=True)
-        ) if not bidirectional else None
+        if self._rnn_type == 'cnn':
+            def _block(in_channels, out_channels, kernel_size, padding=0, stride=1, bnorm=False, bias=True):
+                res = [nn.Conv1d(in_channels=in_channels, out_channels=out_channels,
+                                 kernel_size=kernel_size, padding=padding, stride=stride, bias=bias)]
 
-        fully_connected = nn.Sequential(
-            nn.BatchNorm1d(rnn_hidden_size, momentum=bnm),
-            nn.Linear(rnn_hidden_size, num_classes, bias=False)
-        )
-        self.fc = nn.Sequential(
-            SequenceWise(fully_connected),
-        )
-        self.inference_softmax = InferenceBatchSoftmax()
+                if bnorm:
+                    res.append(nn.BatchNorm1d(out_channels))
+                res.append(nn.ReLU(inplace=True))
+                return res
+
+            size = rnn_hidden_size
+            bnorm = True
+            self.rnns = nn.Sequential(
+                *_block(in_channels=161, out_channels=256, kernel_size=7, padding=3, stride=2, bnorm=bnorm,
+                        bias=not bnorm),
+                *_block(in_channels=256, out_channels=256, kernel_size=7, padding=3, bnorm=bnorm, bias=not bnorm),
+                *_block(in_channels=256, out_channels=256, kernel_size=7, padding=3, bnorm=bnorm, bias=not bnorm),
+                *_block(in_channels=256, out_channels=256, kernel_size=7, padding=3, bnorm=bnorm, bias=not bnorm),
+                *_block(in_channels=256, out_channels=256, kernel_size=7, padding=3, bnorm=bnorm, bias=not bnorm),
+                *_block(in_channels=256, out_channels=256, kernel_size=7, padding=3, bnorm=bnorm, bias=not bnorm),
+                *_block(in_channels=256, out_channels=256, kernel_size=7, padding=3, bnorm=bnorm, bias=not bnorm),
+                *_block(in_channels=256, out_channels=256, kernel_size=7, padding=3, bnorm=bnorm, bias=not bnorm),
+                *_block(in_channels=256, out_channels=size, kernel_size=7, padding=3, bnorm=bnorm, bias=not bnorm),
+                *_block(in_channels=size, out_channels=size, kernel_size=1, bnorm=bnorm, bias=not bnorm),
+            )
+            self.fc = nn.Sequential(
+                nn.Conv1d(in_channels=size, out_channels=num_classes, kernel_size=1)
+            )
+        else:
+            # Based on above convolutions and spectrogram size using conv formula (W - F + 2P)/ S+1
+            rnn_input_size = int(math.floor((sample_rate * window_size + 1e-2) / 2) + 1)
+            rnn_input_size = int(math.floor(rnn_input_size + 2 * 20 - 41 + 1e-2) / 2 + 1)
+            rnn_input_size = int(math.floor(rnn_input_size + 2 * 10 - 21 + 1e-2) / 2 + 1)
+            rnn_input_size *= 32
+
+            rnns = []
+            rnn = BatchRNN(input_size=rnn_input_size, hidden_size=rnn_hidden_size, rnn_type=supported_rnns[rnn_type],
+                           bidirectional=bidirectional, batch_norm=False)
+            rnns.append(('0', rnn))
+            for x in range(nb_layers - 1):
+                rnn = BatchRNN(input_size=rnn_hidden_size, hidden_size=rnn_hidden_size,
+                               rnn_type=supported_rnns[rnn_type],
+                               bidirectional=bidirectional, bnm=bnm)
+                rnns.append(('%d' % (x + 1), rnn))
+            self.rnns = nn.Sequential(OrderedDict(rnns))
+
+            self.lookahead = nn.Sequential(
+                # consider adding batch norm?
+                Lookahead(rnn_hidden_size, context=context),
+                nn.Hardtanh(0, 20, inplace=True)
+            ) if not bidirectional else None
+
+            fully_connected = nn.Sequential(
+                nn.BatchNorm1d(rnn_hidden_size, momentum=bnm),
+                nn.Linear(rnn_hidden_size, num_classes, bias=False)
+            )
+            self.fc = nn.Sequential(
+                SequenceWise(fully_connected),
+            )
 
     def forward(self, x, lengths):
         assert x.is_cuda
         lengths = lengths.cpu().int()
         output_lengths = self.get_seq_lens(lengths).cuda()
-        x = self.dropout1(x)
-        x, _ = self.conv(x, output_lengths)
-        x = self.dropout2(x)
-        assert x.is_cuda
-        # x = x.to('cuda')
 
-        sizes = x.size()
-        x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  # Collapse feature dimension
-        x = x.transpose(1, 2).transpose(0, 1).contiguous()  # TxNxH
-        assert x.is_cuda
-
-        for rnn in self.rnns:
-            x = rnn(x, output_lengths)
+        if self._rnn_type == 'cnn':
+            x = x.squeeze(1)
+            x = self.rnns(x)
+            x = self.fc(x)
+            x = x.transpose(1, 2).transpose(0, 1).contiguous()
+        else:
+            # x = self.dropout1(x)
+            x, _ = self.conv(x, output_lengths)
+            # x = self.dropout2(x)
+            if DEBUG: assert x.is_cuda
+            # x = x.to('cuda')
+            sizes = x.size()
+            x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  # Collapse feature dimension
+            x = x.transpose(1, 2).transpose(0, 1).contiguous()  # TxNxH
             assert x.is_cuda
 
-        if not self._bidirectional:  # no need for lookahead layer in bidirectional
-            x = self.lookahead(x)
-            assert x.is_cuda
+            for rnn in self.rnns:
+                x = rnn(x, output_lengths)
+                assert x.is_cuda
 
-        x = self.fc(x)
-        assert x.is_cuda
+            if not self._bidirectional:  # no need for lookahead layer in bidirectional
+                x = self.lookahead(x)
+                assert x.is_cuda
+
+            x = self.fc(x)
+        if DEBUG: assert x.is_cuda
         x = x.transpose(0, 1)
         # identity in training mode, softmax in eval mode
-        x = self.inference_softmax(x)
-        assert x.is_cuda
-        assert output_lengths.is_cuda
-        return x, output_lengths
+        outs = F.softmax(x, dim=-1)
+        if DEBUG: assert outs.is_cuda
+        if DEBUG: assert output_lengths.is_cuda
+        return x, outs, output_lengths
 
     def get_seq_lens(self, input_length):
         """
@@ -288,12 +321,13 @@ class DeepSpeech(nn.Module):
                     nb_layers=package['hidden_layers'],
                     labels=package['labels'],
                     audio_conf=package['audio_conf'],
-                    rnn_type=supported_rnns[package['rnn_type']],
+                    rnn_type=package['rnn_type'],
                     bnm=package.get('bnm', 0.1),
                     bidirectional=package.get('bidirectional', True))
         model.load_state_dict(package['state_dict'])
-        for x in model.rnns:
-            x.flatten_parameters()
+        if package['rnn_type'] != 'cnn':
+            for x in model.rnns:
+                x.flatten_parameters()
         return model
 
     @classmethod
@@ -302,7 +336,7 @@ class DeepSpeech(nn.Module):
                     nb_layers=package['hidden_layers'],
                     labels=package['labels'],
                     audio_conf=package['audio_conf'],
-                    rnn_type=supported_rnns[package['rnn_type']],
+                    rnn_type=package['rnn_type'],
                     bnm=package.get('bnm', 0.1),
                     bidirectional=package.get('bidirectional', True))
         model.load_state_dict(package['state_dict'])
@@ -317,7 +351,7 @@ class DeepSpeech(nn.Module):
             'version': model._version,
             'hidden_size': model._hidden_size,
             'hidden_layers': model._hidden_layers,
-            'rnn_type': supported_rnns_inv.get(model._rnn_type, model._rnn_type.__name__.lower()),
+            'rnn_type': model._rnn_type,
             'audio_conf': model._audio_conf,
             'labels': model._labels,
             'state_dict': model.state_dict(),
@@ -369,7 +403,7 @@ class DeepSpeech(nn.Module):
             "version": m._version,
             "hidden_size": m._hidden_size,
             "hidden_layers": m._hidden_layers,
-            "rnn_type": supported_rnns_inv[m._rnn_type]
+            "rnn_type": m._rnn_type
         }
         return meta
 
@@ -392,7 +426,7 @@ def main():
     print("DeepSpeech version: ", model._version)
     print("")
     print("Recurrent Neural Network Properties")
-    print("  RNN Type:         ", model._rnn_type.__name__.lower())
+    print("  RNN Type:         ", model._rnn_type)
     print("  RNN Layers:       ", model._hidden_layers)
     print("  RNN Size:         ", model._hidden_size)
     print("  Classes:          ", len(model._labels))

test.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
         inputs = inputs.to(device)
 
         # print(inputs.shape, inputs.is_cuda, input_sizes.shape, input_sizes.is_cuda)
-        out, output_sizes = model(inputs, input_sizes)
+        out0, out, output_sizes = model(inputs, input_sizes)
 
         del inputs, targets, input_percentages, target_sizes
 
@@ -136,7 +136,7 @@ if __name__ == '__main__':
             num_tokens += wer_ref
             num_chars += cer_ref
 
-        del out, output_sizes
+        del out, out0, output_sizes
         if (i + 1) % 5 == 0:
             gc.collect()
             torch.cuda.empty_cache()

train.py
@@ -24,7 +24,7 @@ parser.add_argument('--train-manifest', metavar='DIR',
 parser.add_argument('--val-manifest', metavar='DIR',
                     help='path to validation manifest csv', default='data/val_manifest.csv')
 parser.add_argument('--curriculum', metavar='DIR',
-                    help='path to curriculum file', default='data/curriculum.csv')
+                    help='path to curriculum file', default='')
 parser.add_argument('--sample-rate', default=16000, type=int, help='Sample rate')
 parser.add_argument('--batch-size', default=20, type=int, help='Batch size for training')
 parser.add_argument('--num-workers', default=4, type=int, help='Number of workers used in data-loading')
@@ -293,7 +293,7 @@ def check_model_quality(epoch, checkpoint, train_loss, train_cer, train_wer):
     gc.collect()
     torch.cuda.empty_cache()
 
-    val_cer_sum, val_wer_sum, valid_loss = 0, 0, 0
+    val_cer_sum, val_wer_sum, val_loss_sum = 0, 0, 0
     num_chars, num_words, num_losses = 0, 0, 0
     model.eval()
     with torch.no_grad():
@@ -310,7 +310,7 @@ def check_model_quality(epoch, checkpoint, train_loss, train_cer, train_wer):
 
             inputs = inputs.to(device)
 
-            out, output_sizes = model(inputs, input_sizes)
+            out, outs, output_sizes = model(inputs, input_sizes)
 
             loss = criterion(out.transpose(0, 1), targets, output_sizes.cpu(), target_sizes)
             loss = loss / inputs.size(0)  # average the loss by minibatch
@@ -323,8 +323,8 @@ def check_model_quality(epoch, checkpoint, train_loss, train_cer, train_wer):
                 print("WARNING: received an inf loss, setting loss value to 1000")
                 loss_value = 1000
             loss_value = float(loss_value)
-            valid_loss = (valid_loss * 0.998 + loss_value * 0.002)  # discount earlier losses
-            valid_loss += loss_value
+            val_loss_sum = (val_loss_sum * 0.998 + loss_value * 0.002)  # discount earlier losses
+            val_loss_sum += loss_value
             num_losses += 1
 
             decoded_output, _ = decoder.decode(out, output_sizes)
@@ -339,7 +339,7 @@ def check_model_quality(epoch, checkpoint, train_loss, train_cer, train_wer):
                 num_chars += cer_ref
 
             del inputs, targets, input_percentages, target_sizes
-            del out, output_sizes, input_sizes
+            del out, outs, output_sizes, input_sizes
             del split_targets, loss
 
             if args.cuda:
@@ -351,25 +351,28 @@ def check_model_quality(epoch, checkpoint, train_loss, train_cer, train_wer):
               'Average WER {wer:.3f}\t'
               'Average CER {cer:.3f}\t'.format(epoch + 1, wer=val_wer, cer=val_cer))
 
-        avg_loss = valid_loss / num_losses
+        val_loss = val_loss_sum / num_losses
         plots.loss_results[epoch] = train_loss
         plots.wer_results[epoch] = train_wer
         plots.cer_results[epoch] = train_cer
         plots.epochs[epoch] = epoch + 1
 
-        checkpoint_plots.loss_results[checkpoint] = train_loss
+        checkpoint_plots.loss_results[checkpoint] = val_loss
         checkpoint_plots.wer_results[checkpoint] = val_wer
         checkpoint_plots.cer_results[checkpoint] = val_cer
         checkpoint_plots.epochs[checkpoint] = checkpoint + 1
 
         plots.plot_progress(epoch, train_loss, train_cer, train_wer)
-        checkpoint_plots.plot_progress(checkpoint, avg_loss, val_cer, val_wer)
+        checkpoint_plots.plot_progress(checkpoint, val_loss, val_cer, val_wer)
 
         if args.checkpoint_anneal != 1.0:
             global lr_plots
-            lr_plots.loss_results[checkpoint] = avg_loss
-            lr_plots.epochs[checkpoint:] = get_lr()
-            lr_plots.plot_progress(checkpoint, avg_loss, val_cer, val_wer)
+            lr_plots.loss_results[checkpoint] = val_loss
+            lr_plots.epochs[checkpoint] = get_lr()
+            zero_loss = lr_plots.loss_results == 0
+            lr_plots.loss_results[zero_loss] = val_loss
+            lr_plots.epochs[zero_loss] = get_lr()
+            lr_plots.plot_progress(checkpoint, val_loss, val_cer, val_wer)
     return val_wer, val_cer
 
 
@@ -402,8 +405,9 @@ class Trainer:
         inputs = inputs.to(device)
         input_sizes = input_sizes.to(device)
 
-        out, output_sizes = model(inputs, input_sizes)
+        out, outs, output_sizes = model(inputs, input_sizes)
         assert out.is_cuda
+        assert outs.is_cuda
         assert output_sizes.is_cuda
 
         split_targets = []
@@ -474,7 +478,7 @@ class Trainer:
                 batch_time=batch_time, data_time=data_time, loss=losses))
 
         del inputs, targets, input_percentages, input_sizes
-        del out, output_sizes, target_sizes, loss
+        del out, outs, output_sizes, target_sizes, loss
         return loss_value
 
 
@@ -518,7 +522,7 @@ def train(from_epoch, from_iter, from_checkpoint):
             total_loss += trainer.train_batch(epoch, i, data)
             num_losses += 1
 
-            if (i + 1) % 5 == 0:
+            if (i + 1) % 10 == 0:
                 # deal with GPU memory fragmentation
                 gc.collect()
                 torch.cuda.empty_cache()
@@ -613,8 +617,8 @@ if __name__ == '__main__':
     save_folder = args.save_folder
     os.makedirs(save_folder, exist_ok=True)
 
-    plots = PlotWindow(args.id, 'epochs', log_y=True)
-    checkpoint_plots = PlotWindow(args.id, 'checkpoint', log_y=True)
+    plots = PlotWindow(args.id, 'train loss, epochs', log_y=True)
+    checkpoint_plots = PlotWindow(args.id, 'val loss, checkpoints', log_y=True)
     lr_plots = LRPlotWindow(args.id, 'LRFinder', log_x=True)
 
     total_avg_loss, start_epoch, start_iter, start_checkpoint = 0, 0, 0, 0
@@ -646,7 +650,7 @@ if __name__ == '__main__':
                 checkpoint_plots.loss_results = package.get('checkpoint_loss_results', torch.Tensor(10000))
                 checkpoint_plots.cer_results = package.get('checkpoint_cer_results', torch.Tensor(10000))
                 checkpoint_plots.wer_results = package.get('checkpoint_wer_results', torch.Tensor(10000))
-            if package['loss_results'] is not None and start_epoch > 0:
+            if package['cer_results'] is not None and start_epoch > 0:
                 plots.plot_history(start_epoch)
             if package.get('checkpoint_cer_results') is not None and start_checkpoint > 0:
                 checkpoint_plots.plot_history(start_checkpoint)
@@ -667,7 +671,7 @@ if __name__ == '__main__':
         model = DeepSpeech(rnn_hidden_size=args.hidden_size,
                            nb_layers=args.hidden_layers,
                            labels=labels,
-                           rnn_type=supported_rnns[rnn_type],
+                           rnn_type=rnn_type,
                            audio_conf=audio_conf,
                            bidirectional=args.bidirectional,
                            bnm=args.batch_norm_momentum)
@@ -678,7 +682,8 @@ if __name__ == '__main__':
     decoder = GreedyDecoder(labels)
     train_dataset = SpectrogramDataset(audio_conf=audio_conf,
                                        manifest_filepath=args.train_manifest,
-                                       labels=labels, normalize=args.norm, augment=args.augment)
+                                       labels=labels, normalize=args.norm, augment=args.augment,
+                                       curriculum_filepath=args.curriculum)
     test_dataset = SpectrogramDataset(audio_conf=audio_conf,
                                       manifest_filepath=args.val_manifest,
                                       labels=labels, normalize=args.norm, augment=False)
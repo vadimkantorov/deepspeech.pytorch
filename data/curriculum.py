412fa89109a04b259fedf6d6a904a1286733848a
snakers41 <aveysov@gmail.com>
2019-04-28 15:24:32 +0000

    Tweak CR params

--- data/curriculum.py
+++ data/curriculum.py
@@ -3,8 +3,9 @@ import random
 
 class Curriculum:
     BASE_PROB = 0.02
-    SHORT_PROB = 0.05
+    SHORT_PROB = 0.00 # 0.05
     CL_PROB = 0.2
+    CL_POINT = 0.2
 
     @classmethod
     def sample(cls, items, getter, epoch, min=1):
@@ -22,10 +23,10 @@ class Curriculum:
     def get_prob(cls, text, cer):
         length_bonus = cls.SHORT_PROB * 3 / (3 + len(text))
         cl_prob = 0
-        if cer < 0.1:
-            cl_prob = (0 + cer) / (0 + 0.1)
+        if cer < cls.CL_POINT:
+            cl_prob = (0 + cer) / (0 + cls.CL_POINT)
         elif cer < 0.51:
-            cl_prob = (0.51 - cer) / (0.51 - 0.1)
+            cl_prob = (0.51 - cer) / (0.51 - cls.CL_POINT)
         cl_bonus = cls.CL_PROB * cl_prob
         return cls.BASE_PROB + length_bonus + cl_bonus
 
